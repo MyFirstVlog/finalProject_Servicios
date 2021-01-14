e3d21771763fb695b088151b9cc009eda05850ee
alejo <alejoestradam@yahoo.com>
2021-01-14 01:37:32 -0500

    Se agrego listas para pagos multiples

--- Licoreria.py
+++ Licoreria.py
@@ -1,5 +1,7 @@
 from SocketServer import ThreadingTCPServer, BaseRequestHandler, UDPServer
 import socket
+from numpy import *
+import numpy as np
 
 class Licoreria:
     def __init__(self, codigo, procedencia, unidades, costo):
@@ -14,7 +16,11 @@ class Licoreria:
         valorPagar = cantidad * self.cost         
         return valorPagar
     def restaUnidades(self, cantidad):
-        self.quantity = self.quantity - cantidad
+        if cantidad < self.quantity:
+            self.quantity = self.quantity - cantidad
+            return "si"
+        else:
+            return "no"
 
         
 class MyHandler(BaseRequestHandler):
@@ -22,14 +28,21 @@ class MyHandler(BaseRequestHandler):
         print"Connection from ",str(self.client_address)
         SOCKETS_LIST.append(self.request)                
         host, port = self.client_address
+        columna = 0
+        fila = 0
+        matrizPago = array([["","",0]])
+        listProducto = []
+        dineroPagar = 0
         while True:
             con = 0        
             flag1=2
             flag2=2
             flag3=2  
+            bandera = 2
+            bandera2 = 2
             #flag4=2
             anuncio = self.request.send("La Licoreria de Alejo, lo saluda !!! \n")
-            
+                        
             while flag1 == 2:
                 flag3 = 2
                 anuncio = self.request.send("Comprar o Consultar \n")        
@@ -45,13 +58,26 @@ class MyHandler(BaseRequestHandler):
                         #anuncio = self.request.send("\n") 
                     longitudSockets= len(SOCKETS_LIST)     
                     anuncio = self.request.send("Actualmente hay conectados: " + str(longitudSockets - 1) + " Clientes" + "\n")    
-                if x[0] == "comprar":        
-                    
-                    nombre = x[1]                
+                if x[0] == "comprar":                      
+                    nombre = x[1]                                    
                     for j in listaAlcohol:
                         if(nombre == j.code):
                                 dineroPagar = j.comprar(int(x[2]))
-                                con += dineroPagar                            
+                                con += dineroPagar 
+                                valoIndividual =  j.cost
+                    listProducto=[nombre,dineroPagar,int(x[2])]     
+                    #matrizPago= np.append(listProducto)
+                    if bandera2 == 2:                                         
+                        matrizPago1= append(matrizPago,[listProducto],0)
+                        bandera2 = 1
+                    else:
+                        matrizPago1= append(matrizPago1,[listProducto],0)
+                    #print matrizPago1
+                    if bandera == 2:
+                        matrizPago1= delete(matrizPago1,[0],0)
+                        bandera = 1
+                    lenMatrizRow = len(matrizPago1)
+                    print matrizPago1
                     anuncio = self.request.send("El total a pagar es: "+str(con)+"\n")
                     anuncio = self.request.send("Desea seguir comprando (Y/N) "+"\n")
                     data = self.request.recv(1024)
@@ -69,26 +95,48 @@ class MyHandler(BaseRequestHandler):
                                 port = 8880        
                                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                                 while flag4==2:
-                                    message = str(con) + " "+ data                                   
-                                    sock.sendto(message,("10.0.2.12",8880)) #le manda mensaje a banco cantidad a pagar + nombre + contrasena
-                                    response,remote_host = sock.recvfrom(1024)
-                                    print response                                        
-                                    if response == "si":
-                                        for j in listaAlcohol:
-                                            if(nombre == j.code):
-                                                j.restaUnidades(int(x[2]))
-                                                anuncio = self.request.send("Operacion realizada con exito !!!, tu pedido llegara lo mas pronto posible" + "\n")
-                                                con = 0
-                                                flag3 = 1
-                                                flag4 = 1
-                                                break
-                                    else:
-                                        anuncio = self.request.send("No tienes fondos" + "\n")
-                                        flag3 = 1
-                                        flag4 = 1
-                                        break
-                                                
-                                sock.close() 
+                                    print "entre al while"
+                                    for i in range (0,lenMatrizRow):     
+                                        print "entre al for" , " ",  matrizPago1[i][1]                                 
+                                        message = str(matrizPago1[i][1]) + " "+ data                                   
+                                        sock.sendto(message,("10.0.2.12",8880)) #le manda mensaje a banco cantidad a pagar + nombre + contrasena
+                                        response,remote_host = sock.recvfrom(1024)
+                                        print response 
+                                        rtaCantidad = j.restaUnidades(int(matrizPago1[i][2]))                                      
+                                        if response == "si" and rtaCantidad == "si" :
+                                            for j in listaAlcohol:
+                                                if(matrizPago1[i][0] == j.code):
+                                                    #j.restaUnidades(int(matrizPago1[i][2]))
+                                                    anuncio = self.request.send("Operacion realizada con exito !!!, tu pedido llegara lo mas pronto posible" + "\n")
+                                                    con = 0
+                                                    flag3 = 1
+                                                    flag4 = 1
+                                                    matrizPago = array([["","",0]])
+                                                    bandera = 2
+                                                    bandera2 = 2
+                                                    listProducto = []
+                                                    break
+                                        elif response == "no":
+                                            anuncio = self.request.send("No tienes fondos" + "\n")
+                                            flag3 = 1
+                                            flag4 = 1
+                                            matrizPago = array([["","",0]])
+                                            bandera = 2
+                                            bandera = 2
+                                            listProducto = []
+                                            break   
+                                        elif rtaCantidad == "no":
+                                            anuncio = self.request.send("No tienes Stock" + "\n")
+                                            flag3 = 1
+                                            flag4 = 1
+                                            matrizPago = array([["","",0]])
+                                            bandera = 2
+                                            bandera = 2
+                                            listProducto = []
+                                            break  
+                                                                                   
+                                    sock.close() 
+
                             else:
                                 flag3 = 1                     
                                 
@@ -113,7 +161,7 @@ aguardiente = Licoreria("AguardienteAntioqueno", "Colombia", 9, 43000)
 vino = Licoreria("VinoRose","Francia", 7, 49000)
 
 listaAlcohol = [vodka, ron, whiskey, aguardiente, vino]
-myServer = ThreadingTCPServer(("10.0.2.12",3450), MyHandler)
+myServer = ThreadingTCPServer(("10.0.2.12",3460), MyHandler)
 SOCKETS_LIST = []
 SOCKETS_LIST.append(myServer)
 myServer.serve_forever()
